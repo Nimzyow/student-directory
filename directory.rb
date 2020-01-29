9ad01c133694386511e0d3f44d62902b63ca83b3
Nimzyow <n_soufiani@hotmail.com>
2020-01-29 20:02:18 +0000

    added save functionality

--- directory.rb
+++ directory.rb
@@ -1,22 +1,73 @@
 # Lets put all students into a hash for better readability
 
-def input_students
+def interactive_menu
   students = []
+  loop do
+    print_instructions
+    process(gets.chomp, students)
+  end
+end
+
+def process(selection, students)
+  case selection
+    when "1"
+      students = input_students(students)
+    when "2"
+      show_students(students)
+    when "3"
+      save_students(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def save_students(students)
+  #open file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  students.each do |student|
+    student_data = [student[:name], student[:cohort],
+    student[:hobby], student[:height]]
+    csv_line = student_data.join(",")
+    puts csv_line
+    file.puts csv_line
+    puts "we got to save"
+  end
+  file.close
+end
+
+def print_instructions
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.cvs"
+  puts "9. Exit"
+end
+
+def show_students(students)
+  print_header
+  print_students_list(students)
+  print_footer(students)
+end
+
+def input_students(students)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
   new_student = input_students_properties
   while !new_student[:name].empty? do
-    #the << is called a shovel operator and it's used to put
-    #things into an array
-    students << {name: new_student[:name], hobby: new_student[:hobby],
-      height: new_student[:height], cohort: new_student[:cohort]}
+    enter_student_into_database(new_student, students)
     count_students(students)
-    #get another name from the user
     puts "Enter another students name"
     new_student = input_students_properties
   end
   return students
+end
+
+def enter_student_into_database(new_student, students)
+  #the << is called a shovel operator to put things into an array
+  students << {name: new_student[:name], hobby: new_student[:hobby],
+    height: new_student[:height], cohort: new_student[:cohort]}
 end
 
 def input_students_properties
@@ -49,7 +100,7 @@ def count_students(students)
   puts "Now we have #{students.count} students"
 end
 
-def print(students)
+def print_students_list(students)
   if students.length != 0
     puts "enter cohort. To display all students, simply press Enter"
     cohort_selection = gets.chomp
@@ -83,7 +134,4 @@ def print_footer(names)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu

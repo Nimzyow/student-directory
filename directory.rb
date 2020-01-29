89a4cb2c1e906e9af374cc3922041b5df0dfe2f9
Nimzyow <n_soufiani@hotmail.com>
2020-01-29 18:15:20 +0000

    refactored further to improve readability

--- directory.rb
+++ directory.rb
@@ -1,18 +1,17 @@
 # Lets put all students into a hash for better readability
 
 def input_students
+  students = []
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #create an empty array
-  students = []
   new_student = input_students_properties
   while !new_student[:name].empty? do
     #the << is called a shovel operator and it's used to put
     #things into an array
-
     students << {name: new_student[:name], hobby: new_student[:hobby],
       height: new_student[:height], cohort: new_student[:cohort]}
-    puts "Now we have #{students.count} students"
+    count_students(students)
     #get another name from the user
     puts "Enter another students name"
     new_student = input_students_properties
@@ -28,15 +27,17 @@ def input_students_properties
     if cohort.empty?
       cohort = "november".to_sym
     end
-    puts "Please enter hobby of the student"
-    hobby = gets.chomp
-    puts "Please enter height of the student"
-    height = gets.chomp
-    puts "Please enter country of birth of the student"
+    hobby = input_students_attribute("hobby")
+    height = input_students_attribute("height")
     return {name: name, hobby: hobby, cohort: cohort, height: height}
   else
     return {name: ""}
   end
+end
+
+def input_students_attribute(property)
+  puts "Please enter #{property} of the student"
+  return gets.chomp
 end
 
 def print_header
@@ -44,31 +45,41 @@ def print_header
   puts "--------------"
 end
 
+def count_students(students)
+  puts "Now we have #{students.count} students"
+end
+
 def print(students)
-  puts "enter cohort to display students. If you want to display all students,
-   simply press Enter"
-  cohort_selection = gets.chomp
-  if !cohort_selection.empty?
-    students.map.with_index  {|student, index|
-    #code exercise to print only specific cohort
-      if student[:cohort] == cohort_selection
-          puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)
-                with a height of #{student[:height]} and hobby of
-                #{student[:hobby]}"
-      end}
-  else
-    students.map.with_index  {|student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)
-    with a height of #{student[:height]} and hobby of #{student[:hobby]}"}
+  if students.length != 0
+    puts "enter cohort. To display all students, simply press Enter"
+    cohort_selection = gets.chomp
+    if !cohort_selection.empty?
+      students.map.with_index  {|student, index|
+      #code exercise to print only specific cohort
+        if student[:cohort] == cohort_selection
+            print_message(student, index)
+        end}
+    else
+      students.map.with_index  {|student, index|
+        print_message(student, index)}
+    end
   end
+end
+
+def print_message(student, index)
+  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)
+        with a height of #{student[:height]} and hobby of
+        #{student[:hobby]}"
 end
 
 #Lets print them
 def print_footer(names)
   if names.length != 1
     puts "Overall, we have #{names.count} great students".center(50, "*")
-  else
+  elsif names.length == 1
     puts "Overall, we have #{names.count} great student".center(50, "*")
+  else names.length == 0
+    puts " No students in this academy"
   end
 end
 

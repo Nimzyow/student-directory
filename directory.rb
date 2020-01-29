ea517c7d8f49b8ab4791bd4417d70ca6aa7c0257
Nimzyow <n_soufiani@hotmail.com>
2020-01-29 10:33:41 +0000

    comments added to students directory

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#if we use print followed by print, it doesnt make a new line.
+#finish off with a puts.
+print "Overall, we have "
+print 11
+puts " great students"
